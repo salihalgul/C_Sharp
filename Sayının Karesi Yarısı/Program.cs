5208a3c881924c7abe358932ec7893a9e81831bb
Salih Algül <83082628+salihalgul@users.noreply.github.com>
2022-06-24 23:00:55 +0300

    Add files via upload

---
+++ Sayının Karesi Yarısı/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sayının_Karesi_Yarısı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sayı = 5;
+            Console.WriteLine("Sayı :"+sayı);
+            Console.WriteLine("Sayının karesi :" + sayı * sayı);
+            Console.WriteLine("Sayının yarısı :" + sayı / 2);
+            Console.WriteLine("Sayının 2 ile bölümünden kalanı  " + sayı % 2);
+           
+            Console.ReadLine();
+        }
+    }
+}
