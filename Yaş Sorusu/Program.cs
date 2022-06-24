e510bfa35fbf83388158b8e94e16c8a591a917b9
Salih Algül <83082628+salihalgul@users.noreply.github.com>
2022-06-24 23:04:47 +0300

    Add files via upload

---
+++ Yaş Sorusu/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yaş_Sorusu
+{
+    class Program
+    {
+        //PC : Kaç yaşındasın?
+        //K :45
+        //PC :40 yaşında gösteriyorsunuz
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç yaşındasınız");
+            var yas = Console.ReadLine();
+            int yasim = Convert.ToInt32(yas);
+            yasim = yasim - 5;
+            Console.Write(yasim);
+            Console.Write(" yasinda gösteriyorsunuz");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Yıl-Gün-Ay/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yıl_Gün_Ay
+{
+    class Program
+    {
+        //DateTime sınıfının Today elemanının Year elemanını bir değişkene atayalım
+        //DateTime sınıfının Today elemanının Month elemanını bir değişkene atayalım
+        //DateTime sınıfının Today elemanının Day elemanını bir değişkene atayalım.
+        //Yarının tarihini ekrana yazdırın
+        //28.11.2018
+        static void Main(string[] args)
+        {
+            int yıl = DateTime.Today.Year;
+            int ay = DateTime.Today.Month;
+            int gün = DateTime.Today.Day+1;
+            Console.WriteLine(gün + "." + ay + "." + yıl);
+            Console.ReadLine();
+
+        }
+    }
+}
