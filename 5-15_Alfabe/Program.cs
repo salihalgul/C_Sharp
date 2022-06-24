ffdefbb56b0102bc7fae28811eecaee954920651
Salih Algül <83082628+salihalgul@users.noreply.github.com>
2022-06-24 22:44:21 +0300

    Add files via upload

---
+++ 5-15_Alfabe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_15_Alfabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            alfabe();
+            Console.ReadLine();
+        }
+
+        private static void alfabe()
+        {
+            //Alfabeyi ekrana bastırın
+            for (int i = 'A'; i <= 'Z'; i++)
+            {
+                Console.Write((char)i+" ");
+            }
+        }
+    }
+}
